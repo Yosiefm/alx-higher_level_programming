8d7071cc5cd1e5086865af6a42316f45f1cc2e43
Yosief Mekonnen Teme <yosef@penguin>
2024-03-15 13:18:11 +0300

    task 6 corrected

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     from sys import argv
     from calculator_1 import add, sub, mul, div
-    argv = len(argv)
+    argc = len(argv)
     if argc != 4:
         print('Usage: {} <a> <operator> <b>'.format(argv[0]))
         exit(1)
@@ -17,7 +17,7 @@ if __name__ == "__main__":
         num2 = int(argv[3])
         op = ops[argv[2]]
         result = op(num1, num2)
-        print('{:d} {:s} {:d}'.format(num1, argv[2], num2, result))
+        print('{:d} {:s} {:d} = {:d}'.format(num1, argv[2], num2, result))
     else:
         print('unknown operator. Available operators: +, -, * and /')
         exit(1)
