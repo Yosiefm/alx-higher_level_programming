e89758f5857c190b6cc73293cebdf8c95580bc8f
Yosief Mekonnen Teme <yosef@penguin>
2024-03-18 01:16:15 +0300

    Task Nine9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    new_list = []
-    if my_list:
-        my_list.sort(reverse=True)
-        return (my_list[0])
-    return (None)
-
+    if len(my_list) == 0:
+        return "None"
+    else:
+        max = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > max:
+                max = my_list[i]
+        return max
